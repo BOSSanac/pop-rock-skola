e999b6d52305f02cf1b168fb5fe8146175a75eb9
Coden4me <82325220+Coden4me@users.noreply.github.com>
2021-08-19 12:39:09 +0200

    Update formhandler.php

--- formhandler.php
+++ formhandler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from ='info@poprockskolasarajevo.com'; //domain name
+$email_from ='github.io'; //domain name
 
 $email_subject = "New Form Submission";
 
